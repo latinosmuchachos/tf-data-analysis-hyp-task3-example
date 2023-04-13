fbe5ae9d054da3c58b67facfc5760609b1a17065
latinosmuchachos <89702294+latinosmuchachos@users.noreply.github.com>
2023-04-13 16:25:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
 
 chat_id = 649103283 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(data) -> bool:
     mu = 300
     t_stat, p_value = ttest_1samp(data, mu)
-    alpha = 0.05
+    alpha = 0.02
     if p_value/2 < alpha and t_stat < 0:
         return True
     else:
